2c7b96492bf6400aa7523f90ae4ca4bcd734e129
srj-sooraj <soorajsuresh092@gmail.com>
2025-11-28 16:46:15 +0530

    os module programs completed

--- index5.js
+++ index5.js
@@ -4,9 +4,21 @@ const internal=[];
 const external=[];
 for(const name in network){
     network[name].forEach(element=>{
+        const info={
         interfaces:name,
         ip:element.address,
         mac:element.mac,
         family:element.family
-    })
+        }
+        if(element.interal){
+            internal.push(info);
+        }else{
+            external.push(info);
+        }
+    
+    
+    });
 }
+    console.table(internal)
+    console.table(external)
+    
